d4dc3d616df5bcfa1fcb9c7334da40ce74520d52
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-07-23 20:25:37 -0500

    lessons 1-3

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# a comment, this is so you can read your program later
+
+print "i could have code like this."
+
+# you can also use a comment to "disable"
+# print "this won't run"
+
+print "This will run"
---
+++ ex3.py
@@ -0,0 +1,28 @@
+# prints
+print "i will now count my chickens:"
+# division before addition
+print "Hens", 25+30 / 6.0
+
+# modulo first, then multiplication, then subtraction
+print "Roosters", 100-25 * 3 % 4
+
+# 4%2 = 0, integer division (1/4 = 0)
+print "Now i will count the eggs"
+print 3+2+1-5+4%2-1/4+6
+
+# addition and subtraction before comparisoin
+print "is it true that 3+2<5-7"
+print 3+2 < 5-7
+
+# addition
+print "what is 3+2", 3+2
+
+# subtraction
+print "what is 5-7", 5-7
+print "oh, that's why it is false"
+print "how about some more"
+
+# comparison
+print "is it greater", 5 > -2
+print "is it greater or equal", 5>= -2
+print "is it less or equal", 5 <= -2
