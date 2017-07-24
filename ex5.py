f9ad05e5457126440633b9c2e75cf314365a368a
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-07-23 21:49:25 -0500

    lessons 4 and 5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'bruce szalwinski'
+age = 51
+height = 74
+weight = 210
+eyes = 'hazel'
+teeth = 'white'
+hair = 'black'
+print "let's talk about %s." % name
+print "he's %d inches tall." % height
+print "he's %d pounds heavy." % weight
+print "actually that's not too heavy"
+print "he's got %s eyes and %s hair." % (eyes, hair)
+print "his teeth are usually %s depending on the coffee" % teeth
+
+print "if is add %d, %d and %d i get %d" % (
+    age, height, weight, age + height + weight
+)
