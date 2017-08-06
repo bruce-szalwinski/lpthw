a225ccb38de8bde6f75524b7d18550eade230d7f
Bruce Szalwinski <bruce.szalwinski@cdk.com>
2017-08-06 13:56:02 -0500

    asking questions

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print "How old are you"
+age = raw_input()
+print "How tall are you",
+height = raw_input()
+print "how much do you weigh",
+weight = raw_input()
+
+print "so, you're %r old, %r tall and %r heavy" % (
+    age, height, weight
+)
+
+raw_input("how old are you")
